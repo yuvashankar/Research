0ddc8ca2e66ea459c9c84d5dd05aae1f29313ded
Vinay Yuvashankar <yuvashv@mcmaster.ca>
2016-01-19 13:04:08 -0500

    I have a way of finding the triggers now

--- src/main.c
+++ src/main.c
@@ -8,20 +8,31 @@
 #include <string.h>
 #include <math.h>
 
+#include <limits.h>
+
 #define PRE_EVENT_TIME 1.0
 #define POST_EVENT_TIME 2.0
 
 int OpenFile(const char* fileName, struct edf_hdr_struct *header);
-int AllocateMemory(double* A, int samplesToRead, int numberOfChannels);
+
+unsigned int_to_int(int k) {
+    if (k == 0) return 0;
+    if (k == 1) return 1;                       /* optional */
+    return (k % 2) + 10 * int_to_int(k / 2);
+}
 
 int main(int argc, char const *argv[])
 {
     //Variable Declarations
     int handle,
         numberOfChannels,
-        openFlag;
+        openFlag,
+        readFlag;
 
-    double sampleFrequency;
+    int* rawStatus;
+
+    double sampleFrequency, 
+        triggerTime;
 
     long long numberOfRecords;
     
@@ -36,9 +47,31 @@ int main(int argc, char const *argv[])
     sampleFrequency = ((double)edfHeader.signalparam[1].smp_in_datarecord /
                        (double)edfHeader.datarecord_duration) * EDFLIB_TIME_DIMENSION;
     numberOfChannels = edfHeader.edfsignals;
-    numberOfRecords = edfHeader.signalparam[numberOfChannels - 2].smp_in_datarecord;
-    printf("%lld\n", numberOfRecords);
+    numberOfRecords = edfHeader.signalparam[numberOfChannels - 2].smp_in_file;
 
+    //Allocate Necessary Memory
+    rawStatus = (int*) malloc(numberOfRecords*sizeof(int));
+    assert(rawStatus);
 
+    //Read the File
+    readFlag = edfread_digital_samples(handle, numberOfChannels - 1, numberOfRecords, rawStatus);
+    assert (readFlag != -1);
+    for (int i = 0; i < numberOfRecords; ++i)
+    {
+        printf("%u\n", int_to_int(rawStatus[i]));
+
+        // if ( (rawStatus[i] & 1) != (rawStatus[i-1] & 1) )
+        // {
+        //    triggerTime = ( ((double)i * 
+        //                     (double)edfHeader.file_duration) /
+        //                     ((double)EDFLIB_TIME_DIMENSION * 
+        //                     (double)edfHeader.signalparam[numberOfChannels - 1].smp_in_file));
+        //     printf("Trigger found at %d, at %f, rawStatus[i] = %u \n",i, triggerTime, int_to_int(rawStatus[i]));
+        // }
+    }
+
+    //clean up and close up
+    edfclose_file(handle);
+    free(rawStatus);
     return 0;
 }
