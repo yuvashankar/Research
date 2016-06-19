261b78d68bc1815e8cda39f69c4b27819a8f2b9b
Vinay Yuvashankar <yuvashv@mcmaster.ca>
2016-06-18 21:27:39 -0400

    I'm adding a thing that excludes all of the log files... they're getting pretty big now

--- Morlets/matlab/GenerateTestSignal.m
+++ Morlets/matlab/GenerateTestSignal.m
@@ -1,14 +1,11 @@
-function signal = GenerateTestSignal(samples, FS, FREQ)
+function GenerateTestSignal(samples, amplitude,streach, phase, noise)
 
-dt = 1.0./FS;
-fsig = FREQ/FS;
-dw = 2 * pi * fsig;
-w0 = 0.01;
-one_peri = 1./fsig;
+signal = amplitude * cos( (1:samples) * streach + phase);
 
-signal = zeros(samples, 1);
-
-signal(200:400) = sin( ((200:400) - 200)*dw + w0);
-signal(1000:(1000+2*one_peri) ) = sin( ((1000:1000+2*one_peri) - 1000)*dw + w0);
-signal(2000: (2000+3*one_peri) ) = sin( ((2000:2000+3*one_peri) - 2000)*dw + w0);
+if noise > 0
+    signal = awgn(signal,noise);
+end
+signal = signal';
+% csvwrite('signal.csv', signal);
+save ( 'signal.txt', 'signal', '-ascii');
 end
