313bcfa49dee25eadf22d741accbad3b39cc15b7
Vinay Yuvashankar <yuvashv@mcmaster.ca>
2016-06-30 12:15:55 -0400

    wat

---
+++ gitupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#this is a small script that should update git automatically without me having to pull add commit push"
+
+git pull origin master
+
+git add *
+
+git commit -m $1
+
+git push origin master
+
+
