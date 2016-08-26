596438bd3b0579b20be267bbd20092ab6017c9b4
Vinay Yuvashankar <yuvashv@mcmaster.ca>
2016-08-26 16:46:11 -0400

    looks like we're back where we started, now let's try and start to see what happens when we try to implement that paper

--- Morlets/main.c
+++ Morlets/main.c
@@ -6,14 +6,14 @@
 
 int main(void)
 {
-    double *data, *result, *period;
+    double *data, *result, *wavelet_result, *period;
 
     //Size of Data
     int n = DATA_SIZE;
     int J; 
     double dj, dt, s0;
 
-    dj = 0.0625;
+    dj = 0.125;
 
     dt = 1.0/FS;
     s0 = 2 * dt;
@@ -24,23 +24,15 @@ int main(void)
     //Memory Allocations
     data = malloc(n * sizeof(double));
     result = malloc(J * n * sizeof(double));
+    wavelet_result = malloc( J * n * sizeof(double));
     period = malloc(J * sizeof(double));
     assert(data != NULL); assert(result != NULL); assert(period != NULL);
 
     //populate the data array
-    // int DATA_SIZE = ReadFile(data, "Sample_EEG_data.dat");
-    TestCases(data, 3);
-    // printf("Data Size = %d\n", DATA_SIZE);
+    
+    TestCases(data, 2);
 
-    double mean = gsl_stats_mean(data, 1, DATA_SIZE);
-    double sDeviation = gsl_stats_sd_m(data, 1, DATA_SIZE, mean);
-    // printf("Mean = %f, Standard Deviation = %f\n", mean, sDeviation);
-
-    //Compute the Z-Score or Standard Score
-    for (int i = 0; i < DATA_SIZE; ++i)
-    {
-        data[i] = (data[i] - mean)/sDeviation;
-    }
+    CleanData(data, DATA_SIZE);
 
     //Compute wavelet analysis
     double execution_time = omp_get_wtime();
@@ -55,6 +47,6 @@ int main(void)
 
     
     //sanitation engineering
-    free(data); free(result); free(period);
+    free(data); free(result); free(period); free(wavelet_result);
     return 0;
 }
