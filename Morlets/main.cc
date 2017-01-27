492c6bc2da239cf3b068c6c9fc941d2cacaf9c78
Vinay Yuvashankar <yuvashv@mcmaster.ca>
2017-01-27 12:34:44 -0500

    I found the location where ERSP worked like it should

--- Morlets/main.cc
+++ Morlets/main.cc
@@ -17,7 +17,7 @@ int main(void)
     int n = DATA_SIZE;
     const int J = (int) MAX_I - MIN_I;
 
-    // const int trials = 77;
+    const int trials = 77;
 
     //Memory Allocations
     data    =  (double*) malloc(n *     sizeof(double));
@@ -33,62 +33,62 @@ int main(void)
     scales = GenerateScales(MIN_FREQUENCY, MAX_FREQUENCY, S0);
     frequency = IdentifyFrequencies(scales, J);
 
-    TestCases(data, 8);
+    // TestCases(data, 5);
 
-    // //Populate the data array
-    // for (int i = 0; i < trials; ++i)
-    // {
-    //     TestCases(data, 2);
-    //     for (int j = 0; j < n; ++j)
-    //     {
-    //         data_2D[i * n + j] = data[j];
-    //     }
-    // }
+    //Populate the data array
+    for (int i = 0; i < trials; ++i)
+    {
+        TestCases(data, 5);
+        for (int j = 0; j < n; ++j)
+        {
+            data_2D[i * n + j] = data[j];
+        }
+    }
 
-    CWT_Convolution(data, scales, n, J, 
-                    con_result);
+    // CWT_Convolution(data, scales, n, J, 
+    //                 con_result);
     
-    Wavelet(data, scales, 
-            FS, n, J,
-            wavelet_result);
+    // Wavelet(data, scales, 
+    //         FS, n, J,
+    //         wavelet_result);
 
     // printf("Wavelet_result = %f, con_result = %f\n", wavelet_result[466920], con_result[466920]);
 
-    for (int i = 0; i < n * J; ++i)
-    {
-        // wavelet_result[i] = log(wavelet_result[i]);
+    // for (int i = 0; i < n * J; ++i)
+    // {
+    //     // wavelet_result[i] = log(wavelet_result[i]);
 
-        result[i] = abs(wavelet_result[i] - con_result[i]);
-        // result[i] = abs(wavelet_result[i] - con_result[i])/con_result[i];
-    }
+    //     result[i] = abs(wavelet_result[i] - con_result[i]);
+    //     // result[i] = abs(wavelet_result[i] - con_result[i])/con_result[i];
+    // }
 
-    double max = result[0];
-    int array_index = 0;
-    int freq_index = 0;
+    // double max = result[0];
+    // int array_index = 0;
+    // int freq_index = 0;
 
-    for (int i = 0; i < J; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            if (result[i* n + j] > max)
-            {
-                max = result[i* n + j];
+    // for (int i = 0; i < J; ++i)
+    // {
+    //     for (int j = 0; j < n; ++j)
+    //     {
+    //         if (result[i* n + j] > max)
+    //         {
+    //             max = result[i* n + j];
                 
-                array_index = j;
-                freq_index = i;
-            }
-        }
+    //             array_index = j;
+    //             freq_index = i;
+    //         }
+    //     }
         
-    }
+    // }
 
-    double error_time = (double) array_index/FS;
-    printf("Worst error at %f Hz at %f s\n", frequency[freq_index], error_time);
+    // double error_time = (double) array_index/FS;
+    // printf("Worst error at %f Hz at %f s\n", frequency[freq_index], error_time);
 
 
 
     // Compute the ERSP
-    // ERSP (data_2D, scales, FS, n, J, trials, PAD_FLAG, 
-    // result);
+    ERSP (data_2D, scales, FS, n, J, trials, PAD_FLAG, 
+    result);
 
     // Write to file
     // char filename[] = "DATA.log";
