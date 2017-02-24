c492cc722d1a55c66f72f608dbbfd1712bb939d7
Vinay Yuvashankar <yuvashv@mcmaster.ca>
2017-02-24 07:09:42 -0500

    asdf

--- Morlets/main.cc
+++ Morlets/main.cc
@@ -21,7 +21,7 @@ int main(void)
 
     //Memory Allocations
     data    =  (double*) malloc(n *     sizeof(double));
-    data_2D =  (double*) malloc(n * J * sizeof(double));
+    data_2D =  (double*) malloc(n * trials * sizeof(double));
 
     con_result =     (double*) malloc(n * J * sizeof(double));
     wavelet_result = (double*) malloc(n * J * sizeof(double));
@@ -45,60 +45,13 @@ int main(void)
         }
     }
 
-    // CWT_Convolution(data, scales, n, J, 
-    //                 con_result);
-    
-    // Wavelet(data, scales, 
-    //         FS, n, J,
-    //         wavelet_result);
-
-    // printf("Wavelet_result = %f, con_result = %f\n", wavelet_result[466920], con_result[466920]);
-
-    // for (int i = 0; i < n * J; ++i)
-    // {
-    //     // wavelet_result[i] = log(wavelet_result[i]);
-
-    //     result[i] = abs(wavelet_result[i] - con_result[i]);
-    //     // result[i] = abs(wavelet_result[i] - con_result[i])/con_result[i];
-    // }
-
-    // double max = result[0];
-    // int array_index = 0;
-    // int freq_index = 0;
-
-    // for (int i = 0; i < J; ++i)
-    // {
-    //     for (int j = 0; j < n; ++j)
-    //     {
-    //         if (result[i* n + j] > max)
-    //         {
-    //             max = result[i* n + j];
-                
-    //             array_index = j;
-    //             freq_index = i;
-    //         }
-    //     }
-        
-    // }
-
-    // double error_time = (double) array_index/FS;
-    // printf("Worst error at %f Hz at %f s\n", frequency[freq_index], error_time);
-
-
-
     // Compute the ERSP
     ERSP (data_2D, scales, FS, n, J, trials, PAD_FLAG, 
     result);
 
-    // Write to file
-    // char filename[] = "DATA.log";
-
-
 
     WriteFile(result, frequency, J, n, "DATA.log");
-    // WriteGnuplotScript("dee Herro" , "DATA.log");
-    // Plot(result, frequency,  n, J);
-
+    
     //Free up Memory
     free(data_2D);
     free(data);  free(result);
